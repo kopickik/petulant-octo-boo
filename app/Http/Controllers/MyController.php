a3aaa8a0a37ceb82d251d79edbe2337a67adba2a
Kyle Kopicki <kkopicki@Kyles-MacBook-Pro.local>
2015-07-09 00:19:28 -0400

    second commit

---
+++ app/Http/Controllers/MyController.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created on 7/8/15 at 11:51 PM.
+ * User: kkopicki
+ */
+
+namespace App\Http\Controllers;
+
+
+class MyController
+{
+
+}
---
+++ app/Http/Controllers/MyFirstEloquentObject.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace ;
+
+class MyFirstEloquentObject extends Eloquent {
+    
+    protected fillable = [];
+
+}
